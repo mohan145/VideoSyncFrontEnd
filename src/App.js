1dadf5e3f7bd489796a2e0825e060eb4f184ee09
mohan145 <mohan145krsihna@gmail.com>
2020-05-09 11:12:22 +0530

    Inital React FrontEnd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactPlayer from "react-player";
+import "./App.css";
+import io from "socket.io-client";
+require("dotenv").config();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      playing:false
+    };
+    this.socket=null;
+    this.BACKEND_URL="http://localhost:4000/";
+  }
+
+
+  componentDidMount() {
+
+    console.log(this.BACKEND_URL);
+    
+    this.socket=io(this.BACKEND_URL);
+
+    this.socket.on("message",(data)=>{
+      this.setState({
+        url:data.url
+      })
+    })
+
+    this.socket.on("pause",data=>{
+      console.log("recieved pause");
+      console.log(this.state.playing);
+      this.setState({playing:false});
+      console.log(this.state.playing);
+    })
+
+    this.socket.on("play",(data)=>{
+      console.log("recieved play");
+      this.setState({playing:true});
+    })
+
+    this.socket.on("seekTo",(data)=>{
+      console.log(data);
+      
+      this.player.seekTo(data.duration,"seconds");
+    })
+
+  }
+
+  ref=player=>{
+    this.player=player
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Demo</h1>
+        <ReactPlayer
+          controls
+          ref={this.ref}
+          playing={this.state.playing}
+          url={this.state.url}
+          onReady={() => {
+          }}
+          onStart={() => {
+          }}
+          onPause={() => {
+            console.log("on Pause");
+            this.socket.emit("pause",{"msg":"Pause"});
+          }}
+          onPlay={() => {
+            console.log("on Play");
+            this.socket.emit("play",{"msg":"Play"});
+          }}
+          onSeek={(e)=>{
+            console.log(e);
+            this.socket.emit("seekTo",{"duration":e});
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
+
